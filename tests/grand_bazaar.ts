3bd06e478a93e9d6022812c06832214298bf987a
Sickde <md@metamize.me>
2024-02-16 17:31:28 -0500

    init item account test

--- tests/grand_bazaar.ts
+++ tests/grand_bazaar.ts
@@ -7,6 +7,15 @@ import { readFileSync } from 'fs';
 import { ASSOCIATED_TOKEN_PROGRAM_ID, TOKEN_PROGRAM_ID, createMint, getOrCreateAssociatedTokenAccount } from "@solana/spl-token";
 import { serializeUint64, ByteifyEndianess } from "byteify";
 
+import { PublicKey } from "@solana/web3.js";
+
+import { generateSigner } from '@metaplex-foundation/umi'
+import { createUmi } from '@metaplex-foundation/umi-bundle-defaults';
+import { createTree } from '@metaplex-foundation/mpl-bubblegum'
+
+import {MPL_BUBBLEGUM_PROGRAM_ID, TokenProgramVersion} from "@metaplex-foundation/mpl-bubblegum";
+import { SPL_ACCOUNT_COMPRESSION_PROGRAM_ID, SPL_NOOP_PROGRAM_ID, ValidDepthSizePair, getConcurrentMerkleTreeAccountSize } from "@solana/spl-account-compression";
+
 const connection = new web3.Connection("http://localhost:8899", "confirmed");
 
 const program: Program<GrandBazaar> = new Program<GrandBazaar>(IDL, "BXNayNJzpQoWuAmXbj5gVMAAxVR8HqZWCtokuZM3kVAZ", { connection })
@@ -14,6 +23,8 @@ const program: Program<GrandBazaar> = new Program<GrandBazaar>(IDL, "BXNayNJzpQo
 const SIGNER = web3.Keypair.fromSecretKey(Uint8Array.from(JSON.parse(readFileSync('./keypairs/testing_pair.json').toString())));
 console.log("Using signer: ", SIGNER.publicKey.toString());
 connection.requestAirdrop(SIGNER.publicKey, 100 * web3.LAMPORTS_PER_SOL);
+
+const umi = createUmi(connection);
 
 describe("grand_bazaar", () => {
     const gameId = BigInt(10); //randomU64();
@@ -76,6 +87,78 @@ describe("grand_bazaar", () => {
             sysvarInstructions: web3.SYSVAR_INSTRUCTIONS_PUBKEY,
             rentAccount: web3.SYSVAR_RENT_PUBKEY,
             gameAta: gameATA
+        })
+            .instruction();
+
+        const { blockhash, lastValidBlockHeight } =
+            await connection.getLatestBlockhash();
+
+
+        const msg = new web3.TransactionMessage({
+            payerKey: SIGNER.publicKey,
+            recentBlockhash: blockhash,
+            instructions: [ix],
+        }).compileToV0Message();
+
+        const tx = new web3.VersionedTransaction(msg);
+        tx.sign([SIGNER])
+        console.log(Buffer.from(tx.serialize()).toString("base64"));
+        console.log(await connection.simulateTransaction(tx));
+        const txSig = await connection.sendTransaction(tx)
+        console.log("TX SIG: ", txSig);
+    })
+
+    it("init item account", async () => {
+        
+        //create merkle tree
+        const merkleTree = generateSigner(umi)
+        const builder = await createTree(umi, { 
+            merkleTree,
+            maxDepth: 14, //this tree would allow around 10k item accounts to be minted
+            maxBufferSize: 64,
+        })
+        await builder.sendAndConfirm(umi)
+
+        console.log("Merkle Tree:", merkleTree.publicKey)
+
+        const [treeAuthority, _bump] = PublicKey.findProgramAddressSync(
+            [new PublicKey(merkleTree.publicKey).toBuffer()],
+            new PublicKey("BGUMAp9Gq7iTEuizy4pqaxsTyUCBK68MDfK752saRPUY"),
+        );
+        
+        const gamePdaAddress = web3.PublicKey.findProgramAddressSync(
+            [Buffer.from("game"), gameIdBuffer],
+            program.programId
+        )[0];
+
+        // Item data
+        const accountData = {
+            ItemCollection:"itemCollectionMint",
+            Amount: "1",
+        };
+        const dataStr = JSON.stringify(accountData);
+        const init_data = new TextEncoder().encode(dataStr);
+
+        // has to be smaller than 200 bytes
+        if (init_data.length > 199) {
+            throw new Error("init_data exceeds the maximum allowed length.");
+        }
+
+
+        const ix = await program.methods.mintItemAccount(gameId, init_data).accounts({
+            signer: SIGNER.publicKey,
+            systemProgram: web3.SystemProgram.programId,
+            game: gamePdaAddress,
+            itemCollectionMint:, // mint acc item collection
+            itemCollectionMetadata:, //metadataaccount item collection
+            itemCollectionEdition:, //masteredition account item collection
+            tree_authority: treeAuthority,
+            newLeafOwner: SIGNER.publicKey,
+            merkle_tree: new PublicKey(merkleTree.publicKey),
+            log_wrapper: SPL_NOOP_PROGRAM_ID,
+            compression_program: SPL_ACCOUNT_COMPRESSION_PROGRAM_ID,
+            bubblegum_program: MPL_BUBBLEGUM_PROGRAM_ID,
+            mpl_program: MPL_TOKEN_METADATA_PROGRAM_ID,
         })
             .instruction();
 
