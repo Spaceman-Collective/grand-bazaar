f7c2ef78dbf08563aad2a569963b3ef7eda190fd
spacemandev <devbharel@gmail.com>
2024-02-17 04:34:35 -0700

    wip

--- programs/grand_bazaar/src/instructions/mint_to_item_account.rs
+++ programs/grand_bazaar/src/instructions/mint_to_item_account.rs
@@ -1,16 +1,16 @@
-use crate::*;
+use crate::schemas::CompressedTokenAccount;
 use anchor_lang::prelude::*;
 use mpl_bubblegum::types::MetadataArgs;
-
-use self::schemas::CompressedTokenAccount;
 
 pub fn handler(
     ctx: Context<MintToItemAccount>,
     metadata_args: MetadataArgs,
-    amount_increment: u64,
+    amt_increment: u64,
 ) -> Result<()> {
     let compressed_buffer: Vec<u8> = metadata_args.uri.chars().map(|c| c as u8).collect();
-    //let compressed_token_acc = CompressedTokenAccount::try_from(compressed_buffer.as_slice());
+    let compressed_account: CompressedTokenAccount =
+        flatbuffers::root::<CompressedTokenAccount>(&compressed_buffer).unwrap();
+    let new_amt = compressed_account.amount() + amt_increment;
 
     Ok(())
 }
---
+++ programs/grand_bazaar/src/schemas/mod.rs
@@ -0,0 +1,3 @@
+pub mod compressed_token_account_generated;
+
+pub use compressed_token_account_generated::*;
